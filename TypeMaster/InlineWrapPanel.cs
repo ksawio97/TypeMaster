696cdaf4c51c5ddc2d4b2612f474522db3b500fe
ksawio97 <ksawery9708@gmail.com>
2023-04-20 19:57:22 +0200

    Added TypeTest witch uses span to color text and is aligned by custom panel

---
+++ TypeMaster/InlineWrapPanel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+
+namespace TypeMaster;
+
+public class InlineWrapPanel : Panel
+{
+    public static readonly DependencyProperty LineSpacingProperty = DependencyProperty.Register(
+        "LineSpacing",
+        typeof(double),
+        typeof(InlineWrapPanel),
+        new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsArrange));
+
+    public double LineSpacing
+    {
+        get { return (double)GetValue(LineSpacingProperty); }
+        set { SetValue(LineSpacingProperty, value); }
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        // Measure each child, but constrain the available width to prevent overflow
+        double width = 0;
+        double height = 0;
+        foreach (UIElement child in InternalChildren)
+        {
+            child.Measure(new Size(double.PositiveInfinity, availableSize.Height));
+            if (width + child.DesiredSize.Width > availableSize.Width)
+            {
+                // If the child doesn't fit on the current line, wrap to a new line
+                width = 0;
+                height += child.DesiredSize.Height + LineSpacing; // add line spacing
+            }
+            width += child.DesiredSize.Width;
+        }
+
+        // Return the total desired size
+        return new Size(width, height);
+    }
+
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+        // Arrange each child on the appropriate line, wrapping as necessary
+        double x = 0;
+        double y = 0;
+        double padding = 2; // set the desired padding between each inline element
+        foreach (UIElement child in InternalChildren)
+        {
+            if (x + child.DesiredSize.Width > finalSize.Width)
+            {
+                x = 0;
+                y += child.DesiredSize.Height + padding; // add padding between lines
+            }
+            child.Arrange(new Rect(x, y, child.DesiredSize.Width, child.DesiredSize.Height));
+            x += child.DesiredSize.Width + padding; // add padding between inline elements
+        }
+
+        // Return the final arranged size
+        return finalSize;
+    }
+
+}
--- TypeMaster/ViewModel/TypeTestViewModel.cs
+++ TypeMaster/ViewModel/TypeTestViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace TypeMaster.ViewModel;
 
 public partial class TypeTestViewModel : BaseViewModel
 {
-    [ObservableProperty]
     string? _wikiContent;
 
     [ObservableProperty]
@@ -15,25 +18,63 @@ public partial class TypeTestViewModel : BaseViewModel
 
     WikipediaService wikipediaService;
 
+    public ObservableCollection<Inline> Inlines { get; set; }
+
+
     public TypeTestViewModel()
     {
         wikipediaService = new WikipediaService();
-        
+        Inlines = new ObservableCollection<Inline>();
+
         LoadDataAsync();
     }
-
-    //TO DO on text changed delete input text check correction of input push it to results Textblock (with span colored char) and show in input one less char 
     private async Task LoadDataAsync()
     {
         IsBusy = true;
 
         await Task.Run(async () =>
         {
-            WikiContent = await wikipediaService.TryGetWikipediaPageAsync(600, "en");
+            _wikiContent = await wikipediaService.TryGetWikipediaPageAsync(600, "en");
         });
 
+        UpdateInline();
         IsBusy = false;
     }
+    //TO DO make it more efficent you do not need to update whole text only chars in words
+    partial void OnUserTypeInputChanged(string? value)
+    {
+        UpdateInline(value);
+    }
 
-    
+    void UpdateInline(string? value = null)
+    {
+        string textInputed = value ?? string.Empty;
+        string wikiContent = _wikiContent ?? string.Empty;
+        if (0 < Inlines.Count)
+            Inlines.Clear();
+        Span word = new Span();
+        for (int i = 0; i < wikiContent.Length; i++)
+        {
+            Brush foreground = Brushes.Black;
+            if (i < textInputed.Length)
+            {
+                foreground = textInputed[i] == wikiContent[i] ? Brushes.Green : Brushes.Red;
+            }
+
+            Span character = new Span(new Run(wikiContent[i].ToString()) { Foreground = foreground, FontSize = 20 });
+            if (wikiContent[i] == ' ')
+            {
+                Inlines.Add(word);
+                Inlines.Add(character);
+                word = new Span();
+
+            }
+            else
+            {
+                word.Inlines.Add(character);
+            }
+        }
+        if (!word.Equals(new Span()))
+            Inlines.Add(word);
+    }
 }
